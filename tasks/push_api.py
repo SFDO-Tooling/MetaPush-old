06bd8582cd32f1324c8e3a5011715169fe98f2c1
jofsky <josh.kofsky@gmail.com>
2019-10-16 09:52:53 -0700

    updating for david glick

--- tasks/push_api.py
+++ tasks/push_api.py
@@ -13,12 +13,19 @@ class SyncPushErrors(BaseSalesforceApiTask):
         # Set the namespace option to the value from cumulusci.yml if not already set
         # if "namespace" not in self.options:
         self.options["namespace"] = self.project_config.project__package__namespace
+        self.job_query = "SELECT ID, SubscriberOrganizationKey, (SELECT ErrorDetails, ErrorMessage, ErrorSeverity, ErrorTitle, ErrorType FROM PackagePushErrors) FROM PackagePushJob"
 
     def _run_task(self):
         # Query PackagePushErrors
         ### proper query but wont work need to ask 
         ### 'Select ErrorMessage, ErrorDetails, ErrorTitle, ErrorSeverity, ErrorType from PackagePushError'
         print(self._run_SOQL_query('Select AccountId from USER LIMIT 10'))
+
+        # Get errors
+        formatted_query = self.job_query.format(**self.options)
+        self.logger.debug("Running query for job errors: " + formatted_query)
+        result = self.sf.query(formatted_query)
+        job_records = result["records"]
         # Get heroku postgres service
 
         # service = self.project_config.keychain.get_service("metapush_postgres")
@@ -33,6 +40,7 @@ class SyncPushErrors(BaseSalesforceApiTask):
         res = self.sf.query(query)
         for contact in res['records']:
             self.logger.info('{AccountId}'.format(**contact))
+
 
     # def _run_SOQL_query(self, query):
     # res = self.sf.query(query)
